3bd690022e2963010d826ed7a56446da431f770a
Eleazar Junsan <eljunz@gmail.com>
2018-07-23 17:56:47 +0800

    FIXED: Facebook Preview

--- app.js
+++ app.js
@@ -9,41 +9,51 @@ const fs = require('fs')
 const locals = {}
 let request = require('request')
 
-fs.readFile('data/site.json', (err, data) => {
-  if(err) throw err;
-  let obj = JSON.parse(data);
-  let videoId = obj.content[0].videoid;
-  const url = ''+videoId+'.json'
+function getVideoThumbnail() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("data/site.json", (err, data) => {
+      if (err) throw err;
+      let obj = JSON.parse(data);
+      let videoId = obj.content[0].videoid;
+      const url = "" + videoId + ".json";
 
-  request.get('https://api.vimeo.com/videos/' + videoId, {
-    'auth': {
-      'bearer': '525b876a547a549ea9db0c236918d29b'
-    }
-  }, (e, r,  body)=> {
-    let obj = JSON.parse(body);
-    let images = JSON.stringify(obj.pictures);
-    let items = JSON.parse(images)
-    for (let i = 0; i < items.sizes.length; i++) {
-      if (items.sizes[i].width >= 1024 && items.sizes[i].width < 1400 ) {
-        console.log(items.sizes[i].link);
-        obj = {
-          "title": "vimeo",
-          "url": items.sizes[i].link
+      request.get(
+        "https://api.vimeo.com/videos/" + videoId,
+        {
+          auth: {
+            bearer: "525b876a547a549ea9db0c236918d29b"
+          }
+        },
+        (e, r, body) => {
+          if (e) {
+            reject(e);
+            return;
+          }
+
+          let obj = JSON.parse(body);
+          let images = JSON.stringify(obj.pictures);
+          let items = JSON.parse(images);
+          for (let i = 0; i < items.sizes.length; i++) {
+            if (items.sizes[i].width >= 1024 && items.sizes[i].width < 1400) {
+              obj = {
+                title: "vimeo",
+                url: items.sizes[i].link
+              };
+
+              resolve(obj);
+              return;
+            }
+          }
         }
-        // fs.writeFile('data/video.json', JSON.stringify(obj), (err) =>{
-        //   if(err) throw err;
-        //   console.log("File has been saved!")
-        // })
-        return
-      }
-    }
+      );
+    });
   });
-})
+}
 
 const records = new Records({
   addDataTo: locals,
-  site: { file: 'data/site.json' },
-  video: { file: 'data/video.json' }
+  site: { file: "data/site.json" },
+  video: { callback: getVideoThumbnail }
 });
 
 module.exports = {
@@ -51,7 +61,7 @@ module.exports = {
   ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
     locals: ctx => {
-      return ctx, Object.assign({ pageId: pageId(ctx) }, locals)
+      return ctx, Object.assign({ pageId: pageId(ctx) }, { deployVersion: new Date().getTime() }, locals)
     },
     minify: env === 'production'
   }),
