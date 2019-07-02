1d5c6c66fb7c548116d5828cbebca090fe0a029d
Allen <allen@webriq.services>
2019-07-02 16:09:40 +0800

    add agent bio

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function getVideoThumbnail() {
 const records = new Records({
   addDataTo: locals,
   site: { file: "data/site.json" },
+  biography: { file: "data/bio.json" },
   video: { callback: getVideoThumbnail }
 });
 
